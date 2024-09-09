aaba755afe926ea3edab945d514879422adb27c4
Jorge Garcia Carpio <garciajcarpio@gmail.com>
2024-09-09 14:37:02 +0200

    commit

--- user-app/src/app/components/user-app.component.ts
+++ user-app/src/app/components/user-app.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../models/user';
 import { UserService } from '../services/user.service';
 import { UserComponent } from './user/user.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @Component({
   selector: 'user-app',
   standalone: true,
-  imports: [UserComponent],
+  imports: [UserComponent, UserFormComponent],
   templateUrl: './user-app.component.html'
 })
 export class UserAppComponent implements OnInit{
@@ -15,14 +16,27 @@ export class UserAppComponent implements OnInit{
 
   users: User[] = [];
 
-  constructor(private service: UserService){
+  userSelected: User;
 
+  constructor(private service: UserService){
+    this.userSelected = new User();
   }
   ngOnInit(): void {
     this.service.findAll().subscribe(users => this.users = users);
   }
 
+  addUser(user: User){
+    this.users = [... this.users, {... user}];
+  }
 
+  removeUser(id: number): void{
+    this.users = this.users.filter(user => user.id != id);
+  }
+
+
+  setSelectedUser(user: User): void{
+    this.userSelected = {... user}
+  }
 
 
 }
--- user-app/src/app/components/user/user.component.ts
+++ user-app/src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { User } from '../../models/user';
 
 @Component({
@@ -11,4 +11,19 @@ export class UserComponent {
 
   @Input() users: User[] = [];
   
+  @Output() idUserEventEmitter = new EventEmitter();
+
+  @Output() selectedUserEventEmitter = new EventEmitter();
+
+  onRemoveUser(id: number): void {
+    const confirmEliminar = confirm('¿Esta seguro de que quiere eliminar el usuario?');
+    if(confirmEliminar){
+      this.idUserEventEmitter.emit(id);
+    }
+  }
+
+  onSelectedUser(user: User): void {
+    this.selectedUserEventEmitter.emit(user);
+  }
+
 }
